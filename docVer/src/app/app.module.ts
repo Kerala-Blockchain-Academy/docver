e4b05211b6c1df868d3e4eedaedaa6aa55e74a5c
mycomputer <arjunvnd@gmail.com>
2018-12-13 10:24:19 +0530

    Added list, login

--- docVer/src/app/app.module.ts
+++ docVer/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { ListofComponent } from './listof/listof.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    ListofComponent
   ],
   imports: [
     BrowserModule,
---
+++ docVer/src/app/listof/listof.model.ts
@@ -0,0 +1,10 @@
+export class formlist {
+    public name;
+    public path;
+
+    constructor(name,path){
+        this.name=name;
+        this.path=path;
+    }
+
+}
