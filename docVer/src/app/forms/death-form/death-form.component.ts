98a572ae4469485d0e15f07f675a0fadf0416571
mycomputer <arjunvnd@gmail.com>
2018-12-27 02:07:38 +0530

    forms

--- docVer/src/app/forms/death-form/death-form.component.ts
+++ docVer/src/app/forms/death-form/death-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { SawtoothService } from 'src/app/sawtooth.service';
 
 @Component({
   selector: 'app-death-form',
@@ -9,15 +10,27 @@ import { ActivatedRoute } from '@angular/router';
 export class DeathFormComponent implements OnInit {
   action
   href
-  constructor(private route:ActivatedRoute) { }
+  constructor(private route:ActivatedRoute,private addBlock:SawtoothService) { }
 
   ngOnInit() {
     this.href=this.route.url
     this.action=this.href.value[0].path
     console.log(this.action)
   }
-  onSubmit(form){
-    console.log(this.action,",",form.value)
+
+  onSubmit(bform) {
+    // $event.preventDefault()
+    const data = bform.value;
+    const imp = [0, 1, 2, 3, 4];
+    const docType = 'deathForm';
+    data['imp'] = imp;
+    data['docType'] = docType;
+    const strData = JSON.stringify(data);
+    // const tmp = JSON.parse(strData);
+    // const keys = Object.keys(tmp);
+    // console.log(tmp[keys[imp[0]]]);
+    // this.addBlock.sendData(this.action, strData);
+    this.addBlock.register(this.action, strData);
   }
 
 }
--- docVer/src/app/search/death-search/death-search.component.ts
+++ docVer/src/app/search/death-search/death-search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SawtoothService } from 'src/app/sawtooth.service';
 
 @Component({
   selector: 'app-death-search',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeathSearchComponent implements OnInit {
 
-  constructor() { }
+  constructor(private search:SawtoothService) { }
 
   ngOnInit() {
+  }
+
+
+  onSubmit(bform) {
+    // $event.preventDefault()
+    const data = bform.value;
+    const imp = [0, 1, 2, 3, 4];
+    const docType = 'deathForm';
+    data['imp'] = imp;
+    data['docType'] = docType;
+    const strData = JSON.stringify(data);
+    const path = 'verify';
+    this.search.search(strData, path);
   }
 
 }
